4395a92b34ce64c7720cb19b6b447a2b85672d88
jlf <john.adrift@gmail.com>
2021-11-03 00:16:56 -0400

    Added Unity Time.time timestamp variable - Indicates time block button pressed (_BlockStart)

--- Assets/Scripts/Character/Components/Movement/CharacterBlock.cs
+++ Assets/Scripts/Character/Components/Movement/CharacterBlock.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CharacterBlock : CharacterComponent
 {
     // TODO: ADD CLASS VARIABLES IF NECESSARY
+    private float _BlockStart;  // TODO: SHOULD THIS VARIABLE BE TURNED INTO A GETTER/SETTER?
 
     private CharacterMovement _CharacterMovement;
 
@@ -14,31 +15,37 @@ public class CharacterBlock : CharacterComponent
         _CharacterMovement = GetComponent<CharacterMovement>();
     }
 
-    protected override bool HandlePlayerInput(){
+    protected override bool HandlePlayerInput()
+    {
         if(!base.HandlePlayerInput()) return false;
         
         if(DecideIfCharacterCanBlock()) Block();
-
+        
         return true;   
     }
 
-    private bool DecideIfCharacterCanBlock(){
+    private bool DecideIfCharacterCanBlock()
+    {
         // TODO: CHECK IF BLOCK IS POSSIBLE (MAYBE BASED ON IF PLAYER IS COLLIDING WITH AN ENEMY LAYER/TAGGED OBJECT?)
         if (BlockInput()) return true; // Development output with no condition checking
         return false;
     }
 
-    private bool BlockInput(){
+    private bool BlockInput()
+    {
         if(Input.GetKeyDown(CharacterInputs.BlockKeyCode)) return true;
         return false;
     }
 
-    private void Block(){
-        // TODO: ADD BLOCK FUNCTION
-        Debug.Log(" *** BLOCK FUNCTION ACTIVE *** ");
+    private void Block()
+    {
+        // TODO: ADD BLOCK MECHANICS
+        _BlockStart = Time.time; // Unity internal timestamp for when block button pressed
+        Debug.Log($" *** BLOCK FUNCTION ACTIVE *** {_BlockStart}");
     }
 
-    private float TechnicalPoints(){
+    private float TechnicalPoints()
+    {
         // TODO: BUILD LOGIC FOR TECHNICAL POINT CALCULATION (BASED ON WHEN BLOCK IS ACTIVATED AND COLLIDER CONTACTS PLAYER?)
         return 1f;  // placeholder value
     }
