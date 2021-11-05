79805f417c708dc1a3db9b78a8f3f1f2205113d9
jlf <john.adrift@gmail.com>
2021-11-05 00:16:35 -0400

    Rough mock up of title screen - Scripts/UI folder created for menu controls - Menu created with debug statements - GameStart script created

---
+++ Assets/Scripts/UI/GameStart.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStart : MonoBehaviour
+{
+    public void StartGame ()
+    {
+        Debug.Log("TRIED TO START LEVEL 1");
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void Controls ()
+    {
+        Debug.Log("TRIED TO OPEN CONTROLS SCREEN");
+        SceneManager.LoadScene("Controls");
+    }    
+
+    public void ExitGame ()
+    {
+        Debug.Log("TRIED TO EXIT GAME");
+        Application.Quit();
+    }    
+}
