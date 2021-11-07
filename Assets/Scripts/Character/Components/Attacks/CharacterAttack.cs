1a7bfd39e1e768e6e0dec20256f5635ea86d62b2
Weston Vincze <westonvincze@gmail.com>
2021-11-07 10:24:11 -0500

    Adds filler attack animations to simulate comboing and continues attack logic (WIP still)

--- Assets/Scripts/Character/Components/Attacks/CharacterAttack.cs
+++ Assets/Scripts/Character/Components/Attacks/CharacterAttack.cs
@@ -7,6 +7,23 @@ public class CharacterAttack: CharacterComponent
     private LayerMask _EnemyLayers;
     private float _AttackRange = 0.5f;
     private Transform _AttackPoint;
+
+
+    private bool _IsAttacking = false;
+    private int _MaxComboCount = 3;
+    private int _CurrentComboCount = 0;
+    private float _TimeUntilCharacterCanAttack = 0f;
+
+    // We need to track:
+    // - attack delays (i.e, when the player has to wait before making an attack. 
+    // - time until combo resets
+    // - total attacks in combo
+    // - current combo count
+    // each attack should have:
+    //   - damage
+    //   - range?
+    //   - tech point value
+    //   - end lag value
     
     protected override void Start()
     {
@@ -17,7 +34,10 @@ public class CharacterAttack: CharacterComponent
 
     protected override bool HandlePlayerInput()
     {
-        if(!base.HandlePlayerInput()) return false;        
+        if (!base.HandlePlayerInput()) return false;
+
+        if (DecideIfCharacterCanAttack()) Attack();
+
         return true;
     }
 
@@ -27,13 +47,20 @@ public class CharacterAttack: CharacterComponent
 
     }
 
-    private bool DecideIfCharacterCanAttack()
-    {
-        return false;
-    }
-
     private void Attack()
     {
+        if (_CurrentComboCount == _MaxComboCount)
+        {
+            _CurrentComboCount = 0;
+        } 
+        else
+        {
+            _CurrentComboCount++;
+        }
+        //_TimeUntilCharacterCanAttack = Time.time + 0.3333f;
+        _Animation.ChangeAnimationState(string.Format("Attack{0}", _CurrentComboCount), CharacterAnimation.AnimationType.Static);
+
+        if (!_AttackPoint) return;
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(_AttackPoint.position, _AttackRange, _EnemyLayers);
 
         foreach(Collider2D enemy in hitEnemies)
@@ -56,5 +83,15 @@ public class CharacterAttack: CharacterComponent
         // TODO: BUILD LOGIC FOR TECHNICAL POINT CALCULATION (BASED ON WHEN BLOCK IS ACTIVATED AND COLLIDER CONTACTS PLAYER?)
         
         return 1f;  // placeholder value for tech points given after function completes
+    }
+
+    private bool DecideIfCharacterCanAttack()
+    {
+        return AttackInput() && Time.time <= _TimeUntilCharacterCanAttack;
+    }
+
+    private bool AttackInput()
+    {
+        return (Input.GetKeyDown(CharacterInputs.AttackKeyCode));
     }
 }
