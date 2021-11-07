4ccf62800fdb9dd49e1b00b59630e5de5d491d34
Weston Vincze <westonvincze@gmail.com>
2021-11-07 16:18:57 -0500

    Adds generic wind particle effect with rotation

---
+++ Assets/Scripts/Environment/Effects/WindVFX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Environment.Effects
+{
+    public class WindVFX : MonoBehaviour
+    {
+        [SerializeField]
+        ParticleSystem[] _Effects;
+        // what can we control?
+        // direction
+        // width
+        // intensity (optional)
+
+
+        // random material (small/large wind) alternation
+
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
